697aacdff7a2f6682b69a97341e280b1b90a2fb1
Yash292006 <chavanyash1429@gmail.com>
2025-11-11 10:36:56 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,18 +1,24 @@
 import sys
-if len(sys.argv) ==2:
-    script_name = temp.argv[0]
-    temp=temp.argv[1]
+
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]  # Fixed: changed temp.argv to sys.argv
+    temp = sys.argv[1]         # Fixed: changed temp.argv to sys.argv
     print("user provided inputs")
 else:
-    script_name=sys.argv[0]
-    temp="40"
+    script_name = sys.argv[0]
+    temp = "40"
     print("default inputs")
-if temp<"15":
-    state="cold"
-elif temp<"15"and temp>"30":
-    state="normal"
+
+# Fixed: Convert temp to integer for proper comparison and fixed logical conditions
+temp_int = int(temp)
+
+if temp_int < 15:
+    state = "cold"
+elif temp_int >= 15 and temp_int <= 30:  # Fixed: changed conditions
+    state = "normal"
 else:
-    state="hot"
-print("Script Name:",script_name)
-print("Temperature:",temp)
-print("State:",state)
+    state = "hot"
+
+print("Script Name:", script_name)
+print("Temperature:", temp)
+print("State:", state)
